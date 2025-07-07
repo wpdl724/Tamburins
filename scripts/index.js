8024097f953631a2dc8fa63c08cb98ad07a4d6eb
jay <wpdl724@gmail.com>
2025-07-07 14:50:49 +0900

    헤더,네비 위치 재정렬

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+// 헤더 .js 
+// 헤더 lnb , li  마우스 호버했을때 내려오게 제작
+
+const h_gnb = document.querySelectorAll('.category > li');
+const h_lnb = document.querySelectorAll('.lnb_bg');
+console.log(h_gnb,h_lnb );
+
