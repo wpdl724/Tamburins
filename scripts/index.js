70f107a9d30b79958b3023cbf9561c6cedce7e7e
jay <wpdl724@gmail.com>
2025-07-07 18:15:52 +0900

    2행 슬라이더 진행

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,38 @@
-// 헤더 .js 
-// 헤더 lnb , li  마우스 호버했을때 내려오게 제작
 
-const h_gnb = document.querySelectorAll('.category > li');
-const h_lnb = document.querySelectorAll('.lnb_bg');
-console.log(h_gnb,h_lnb );
+/* 마우스 올렸을때 lnb 나타나는 효과 */
+document.addEventListener("DOMContentLoaded", function () {
+    const menuItems = document.querySelectorAll(".category > li");
+    const lnbItems = document.querySelectorAll(".lnb_bg");
 
+    for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("mouseenter", function () {
+        // 모든 lnb_bg 숨김 처리
+        for (let j = 0; j < lnbItems.length; j++) {
+        lnbItems[j].style.display = "none";
+        }
+
+        // 현재 메뉴의 lnb만 보여줌
+        const currentLnb = menuItems[i].querySelector(".lnb_bg");
+        if (currentLnb) {
+        currentLnb.style.display = "block";
+        }
+    });
+
+    menuItems[i].addEventListener("mouseleave", function () {
+        const currentLnb = menuItems[i].querySelector(".lnb_bg");
+        if (currentLnb) {
+        currentLnb.style.display = "none";
+        }
+    });
+    }
+});
+
+/* 띠배너 X표시 클릭시 없어지는 효과 */
+
+
+/* 2행 _ 신제품 스와이퍼 */
+var mySwiper = new Swiper('.swiper-container', { 
+    //필요한 옵션을 작성합니다.
+    //옵션 문법
+    //옵션:값, 옵션:값
+})
