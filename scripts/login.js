1af106d4629ce80d2208a06983fc9d6dbfdfc755
jay <wpdl724@gmail.com>
2025-08-06 19:10:23 +0900

    로그인 폼 수정

--- scripts/login.js
+++ scripts/login.js
@@ -7,3 +7,38 @@ document.querySelectorAll('a').forEach(function(link) {
     });
 });
 
+
+
+/* login 폼 */
+
+const loginTabBtn = document.querySelector('.tab_login_btn');
+const nonLoginTabBtn = document.querySelector('.tab_nonlogin_btn');
+
+const loginForm = document.getElementById('login_form');
+const nonLoginForm = document.getElementById('nonlogin_form');
+
+// 로그인 탭 클릭 시
+loginTabBtn.addEventListener('click', function (e) {
+e.preventDefault();
+
+// 탭 스타일 조정
+loginTabBtn.classList.add('active');
+nonLoginTabBtn.classList.remove('active');
+
+// 폼 전환
+loginForm.style.display = 'block';
+nonLoginForm.style.display = 'none';
+});
+
+// 비회원 탭 클릭 시
+nonLoginTabBtn.addEventListener('click', function (e) {
+e.preventDefault();
+
+// 탭 스타일 조정
+loginTabBtn.classList.remove('active');
+nonLoginTabBtn.classList.add('active');
+
+// 폼 전환
+loginForm.style.display = 'none';
+nonLoginForm.style.display = 'block';
+});
